07598d6c58335bb2accba776d588cadc15100bb9
Andras Nemeth <xxandreww@gmail.com>
2011-07-31 20:09:45 +0200

    As created by rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gtabs::Application.config.secret_token = 'da78c7fb8c314b37d6a48bd959272ac04da8c24a7e075b1613855cb2ea38347662aa4bfc851fc8c988238949138f44faf8074f44b4c44b3a6ecee93dfa22ff6f'
